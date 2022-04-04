bb423e118438b8ed756c1897b26b86c289e2f15f
Jacob Roman <jcbrmn06@gmail.com>
2022-04-04 12:54:43 -0400

    docker stuff

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM node:17.1.0
 WORKDIR /app/medusa
 
 COPY package.json .
-COPY develop.sh .
-COPY yarn.lock .
 
 RUN apt-get update
 
@@ -14,6 +12,8 @@ RUN npm install -g npm@latest
 
 RUN npm install -g @medusajs/medusa-cli@latest
 
-RUN npm install
+RUN npm install --loglevel=error
 
-ENTRYPOINT ["./develop.sh"]
+COPY . .
+
+ENTRYPOINT ["./develop.sh", "develop"]
--- medusa-config.js
+++ medusa-config.js
@@ -11,35 +11,20 @@ const DATABASE_URL =
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-// Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
-
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
-  // {
-  //   resolve: `medusa-payment-stripe`,
-  //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
-  //   },
-  // },
 ];
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    database_extra: { ssl: { rejectUnauthorized: false } },
   },
   plugins,
 };
